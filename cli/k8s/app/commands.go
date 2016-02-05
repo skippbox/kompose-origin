65dfb81371152944f0f8c9a180a2efaf3f3a9e65
runseb <runseb@gmail.com>
2016-02-05 17:18:27 +0100

    Fix env parsin

--- cli/k8s/app/commands.go
+++ cli/k8s/app/commands.go
@@ -269,19 +269,15 @@ func ProjectKuber(p *project.Project, c *cli.Context) {
 
         // Configure the environment variables.
         var envs []api.EnvVar
-        for _, env := range service.Environment {
+        for _, env := range service.Environment.Slice() {
             var character string = "="
             if strings.Contains(env, character) {
                 value := env[strings.Index(env, character) + 1: len(env)]
                 name :=  env[0:strings.Index(env, character)]
-                if err != nil {
-                    logrus.Fatalf("Invalid container env %s for service %s", env, name)
-                }
                 envs = append(envs, api.EnvVar{
                                                Name: name,
                                                Value: value,
-                                              }
-                             )
+                                              })
             } else {
                 logrus.Fatalf("Invalid container env %s for service %s", env, name)
             }
